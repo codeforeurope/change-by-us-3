0f8e60dd8491a3823e6d56b2a43ad5c81fd365ae
Caitlin Morris <caitlinmorris@Caitlins-MacBook-Pro.local>
2013-06-18 16:04:35 -0400

    initial rough outline and view for a user profile page

--- changebyus/user/views.py
+++ changebyus/user/views.py
@@ -10,4 +10,49 @@ from .models import User
 
 user_view = Blueprint('user_view', __name__, url_prefix='/user')
 
+"""
+==============
+User Views
+==============
 
+Web facing user profiles.
+
+"""
+
+@user_view.route('/<display_name>')
+@login_required
+def user_profile_view(display_name):
+    """
+    ABOUT
+        Renders a user profile page
+    METHOD
+        Get
+    INPUT
+        ??       
+    OUTPUT
+        Rendered user profile page
+    PRECONDITIONS
+        User is logged in
+    """
+ 
+    from ..project.api import _get_user_involved_projects
+
+    user =  User.objects(display_name = display_name)
+
+    if user.count() == 0:
+        abort(404)
+
+    first_name = user[0].first_name
+    last_name = user[0].last_name
+    projects = _get_user_involved_projects(user[0].id)
+    email = user[0].email
+    public_email = user[0].public_email
+
+    return render_template('profile.html', 
+        display_name = display_name, 
+        first_name=first_name, 
+        last_name=last_name,
+        projects=projects,
+        email=email,
+        public_email=public_email
+        )

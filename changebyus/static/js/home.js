dc107785304d9fc83be5eb8c59612969eba20a2f
Caitlin Morris <caitlinmorris@Caitlins-MacBook-Pro.local>
2013-09-09 11:09:58 -0400

    rough frontend integration with db, returning projects to basic project list page

---
+++ changebyus/static/js/home.js
@@ -0,0 +1,49 @@
+(function() {
+    var TEMPLATE_URL = '/static';
+    
+    var Project = Backbone.Model.extend({});
+
+    var ProjectList = Backbone.Collection.extend({
+        model: Project,
+        url: '/api/project/list',
+        parse: function(response) {
+            return response.data;
+        }
+    });
+
+    var ProjectView = Backbone.View.extend({         
+        tagName:  "li",
+
+        render: function() {
+            var self = this;
+            $(self.el).template(TEMPLATE_URL + '/templates/main.html', self.model.toJSON(), function() {});
+            return this;
+        }
+    });
+
+    window.MyAppName = Backbone.View.extend({
+        Projects: new ProjectList(),
+
+        initialize: function(options) {
+            var self = this,
+                parentElt = options.appendTo || $('body');
+                
+            TEMPLATE_URL = options.templateUrl || TEMPLATE_URL;
+            
+            parentElt.template(TEMPLATE_URL + '/templates/discover.html', {}, function() {
+                self.Projects.bind('reset', self.addAll, self);
+                self.Projects.fetch();
+            });       
+        },
+
+        addOne: function(todo) {
+            var view = new ProjectView({model: todo});
+            this.$("#project-list").append(view.render().el);
+        },
+
+        addAll: function() {
+            this.Projects.each(this.addOne);
+        },
+    });
+    
+}());

6d6a2c551ad7f2858d885b6d46e513273784044f
Matt Lohmann <mattlohmann@localprojects.net>
2013-09-11 16:40:30 -0400

    partial views first commit and update to template

--- changebyus/static/js/main.js
+++ changebyus/static/js/main.js
@@ -1,18 +1,26 @@
 require.config({
     baseUrl: "/static/js",
     paths: {
-        "jquery": "ext/jquery/jquery", 
+        "jquery":     "ext/jquery/jquery", 
         "underscore": "ext/underscore/underscore-min",
-        "backbone": "ext/backbone/backbone-min", 
-        "bootstrap": "ext/bootstrap/bootstrap",
-        "template": "ext/jquery/template",
-        "app":  "views/CBUAppView"
+        "backbone":   "ext/backbone/backbone-min", 
+        "bootstrap":  "ext/bootstrap/bootstrap",
+        "template":   "ext/jquery/template",
+        "main":       "views/CBUMainView",
+        "discover":   "views/CBUDiscoverView"
     }
 });
 
-require(["jquery","app", "collection/ProjectListCollection"], function($, CBUAppView, ProjectListCollection) {
+require(["jquery","main", "discover","collection/ProjectListCollection"], function($, CBUMainView, CBUDiscoverView) {
     $(document).ready(function() {
-        var projects = new ProjectListCollection();
-        window.CBUAppView = new CBUAppView({ parent:'#frame', collection:projects });
+
+        var path = window.location.pathname;
+
+        if (path.indexOf('/discover')>-1){
+            window.CBUAppView = new CBUDiscoverView({ parent:'#frame' });
+        } else {
+            window.CBUAppView = new CBUMainView({ parent:'#frame' });
+        }
+       
     });
 });
---
+++ changebyus/static/js/views/CBUDiscoverView.js
@@ -0,0 +1,34 @@
+
+define(["underscore", "backbone", "jquery", "template","BannerSearchView"], function(_, Backbone, $, temp, BannerSearchView) {
+    
+    var CBUDiscoverView = Backbone.View.extend({
+
+        parent: 'body',
+        templateDir: '/static',
+        viewData:{},
+        bannerSearchView:null,
+
+        initialize: function(options) {
+            this.templateDir = options.templateDir || this.templateDir;
+            this.parent      = options.parent || this.parent; 
+            this.viewData    = options.viewData || this.viewData; 
+            this.collection  = options.collection || this.collection;
+            this.render();
+        },
+
+        render:function(){
+            // var self = this;
+            this.$el = $("<div class='discover'/>");
+            this.$el.template(this.templateDir + '/templates/discover.html', {data:viewData}, function() {  });
+            $(this.parent).append(this.$el); 
+
+            var bannerParent = this.$el.find(".content");
+            this.bannerSearchView = new BannerSearchView({parent:bannerParent});
+        },
+    });
+
+    return CBUDiscoverView;
+    
+});
+
+

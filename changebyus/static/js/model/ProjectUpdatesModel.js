5cf091351bb2eab2ec9e4f8b0924cf5349fe1309
Matt Lohmann <mattlohmann@localprojects.net>
2013-09-13 13:30:04 -0400

    initial project commit

---
+++ changebyus/static/js/model/ProjectUpdatesModel.js
@@ -0,0 +1,4 @@
+define(["underscore", "backbone"], function(_, Backbone) {
+    var ProjectUpdatesModel = Backbone.Model.extend();
+    return ProjectUpdatesModel;
+});
--- changebyus/static/js/views/partials/ProjectPartialsView.js
+++ changebyus/static/js/views/partials/ProjectPartialsView.js
@@ -5,19 +5,19 @@ define(["underscore", "backbone", "jquery", "template"], function(_, Backbone, $
         parent: 'body',
         templateDir: '/static',
         viewData:{},
+        tagName:  "li",
 
         initialize: function(options) {
             this.templateDir = options.templateDir || this.templateDir;
             this.parent      = options.parent || this.parent; 
-            this.viewData    = options.viewData || this.viewData;  // not sure is this would be the correct one this.model.toJSON()
+            this.viewData    = options.viewData || this.model.toJSON();
             this.render();
         },
 
-        render:function(){
-            //var self = this;
+        render:function(){ 
             this.$el = $("<div class='project-preview'/>");
             this.$el.template(this.templateDir + '/templates/partials-universal/project.html', {data:this.viewData}, function() {});
-            $(this.parent).append(this.$el); 
+            return this;
         }
  
     });

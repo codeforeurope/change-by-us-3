ec29ffe2681211893a86cdd19f576a3e7c04a9ec
Matt Lohmann <mattlohmann@localprojects.net>
2013-09-13 18:27:02 -0400

    calendar fix

--- changebyus/static/js/views/partials/ProjectCalenderView.js
+++ changebyus/static/js/views/partials/ProjectCalenderView.js
@@ -2,11 +2,12 @@ define(["underscore", "backbone", "jquery", "template", "views/partials/ProjectS
     function(_, Backbone, $, temp, ProjectSubView) {
     
     var ProjectCalenderView = ProjectSubView.extend({
-        parent:"#project-calender",
+        parent:"#project-calendar",
         render:function(){
             this.$el = $("<div class='project'/>");
             this.$el.template(this.templateDir + '/templates/partials-project/project-calendar.html', {data:this.viewData}, function() {});
-            $(this.parent).append(this.$el); 
+            $(this.parent).append(this.$el);
+            console.log($(this.parent),this.$el);
         },
         addOne: function(model) {
             var view = new Partial(); //to do 

ff4f38fd71962194ab132acadb29b2a29c5c613d
Matt Lohmann <mattlohmann@localprojects.net>
2013-09-16 11:46:56 -0400

    project toggle

--- changebyus/static/js/views/partials/ProjectCalenderView.js
+++ changebyus/static/js/views/partials/ProjectCalenderView.js
@@ -7,7 +7,7 @@ define(["underscore", "backbone", "jquery", "template", "views/partials/ProjectS
             this.$el = $("<div class='project'/>");
             this.$el.template(this.templateDir + '/templates/partials-project/project-calendar.html', {data:this.viewData}, function() {});
             $(this.parent).append(this.$el);
-            console.log($(this.parent),this.$el);
+            //console.log($(this.parent),this.$el);
         },
         addOne: function(model) {
             var view = new Partial(); //to do 

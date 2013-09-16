317d2d3203ea31da39db7428d380aa32d62d29ac
Matt Lohmann <mattlohmann@localprojects.net>
2013-09-16 14:01:21 -0400

    discussion collections and models

---
+++ changebyus/static/js/collection/ProjectDiscussionCommentsCollection.js
@@ -0,0 +1,12 @@
+define(["underscore", "backbone", "model/ProjectDiscussionCommentModel"], function(_, Backbone, ProjectDiscussionCommentModel) {
+
+    var ProjectDiscussionCommentsCollection = Backbone.Collection.extend({
+        model: ProjectDiscussionCommentModel, 
+        url:"/api/project/"+window.projectID+"/discussion_comments",
+        parse: function(response) {
+            return response.data;
+        }
+    });
+
+    return ProjectDiscussionCommentsCollection;
+});
---
+++ changebyus/static/js/model/ProjectDiscussionCommentModel.js
@@ -0,0 +1,4 @@
+define(["underscore", "backbone"], function(_, Backbone) {
+    var ProjectDiscussionCommentModel = Backbone.Model.extend();
+    return ProjectDiscussionCommentModel;
+});

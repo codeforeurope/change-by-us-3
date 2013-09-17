e78fddc3f04585e2210f84e566f9a47bee73d5c3
Matt Lohmann <mattlohmann@localprojects.net>
2013-09-17 12:29:08 -0400

    passing id

--- changebyus/static/js/collection/ProjectDiscussionCommentsCollection.js
+++ changebyus/static/js/collection/ProjectDiscussionCommentsCollection.js
@@ -1,8 +1,11 @@
 define(["underscore", "backbone", "model/ProjectDiscussionCommentModel"], function(_, Backbone, ProjectDiscussionCommentModel) {
 
     var ProjectDiscussionCommentsCollection = Backbone.Collection.extend({
+    	initialize: function(options) {
+    		this.id = options.id;
+  		}
         model: ProjectDiscussionCommentModel, 
-        url:"/api/project/"+window.projectID+"/discussion_comments",
+        url:"/api/project/"+this.id+"/discussion_comments",
         parse: function(response) {
             return response.data;
         }
--- changebyus/static/js/collection/ProjectDiscussionsCollection.js
+++ changebyus/static/js/collection/ProjectDiscussionsCollection.js
@@ -1,8 +1,11 @@
 define(["underscore", "backbone", "model/ProjectDiscussionModel"], function(_, Backbone, ProjectDiscussionModel) {
 
     var ProjectDiscussionsCollection = Backbone.Collection.extend({
+    	initialize: function(options) {
+    		this.id = options.id;
+  		}
         model: ProjectDiscussionModel, 
-        url:"/api/project/"+window.projectID+"/discussions_list",
+        url:"/api/project/"+this.id+"/discussions_list",
         parse: function(response) {
             return response.data;
         }

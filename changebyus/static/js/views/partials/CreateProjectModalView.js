fc1125389a31374314bcc440f558644692b9cf63
Matt Lohmann <mattlohmann@localprojects.net>
2013-09-17 17:06:07 -0400

    added stubbed out project update, removed any lingering extend layout.html and put in black for the input fields

--- changebyus/static/js/views/partials/CreateProjectModalView.js
+++ changebyus/static/js/views/partials/CreateProjectModalView.js
@@ -3,13 +3,9 @@ define(["underscore", "backbone", "jquery", "template", "views/partials/ProjectS
     
     var CreateProjectModalView = ProjectSubView.extend({
         render:function(){
-            //var self = this;
             this.$el = $("<div class='project-preview'/>");
             this.$el.template(this.templateDir + '/templates/partials-universal/project-create-modal.html', {data:this.viewData}, function() {});
             $(this.parent).append(this.$el); 
-        },
-        loadData:function(){
-            // override in subview
         }
     });
 

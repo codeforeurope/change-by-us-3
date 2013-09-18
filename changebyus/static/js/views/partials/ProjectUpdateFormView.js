10365338996b92177164b7aa669870fcc02653b2
Matt Lohmann <mattlohmann@localprojects.net>
2013-09-18 13:03:29 -0400

    adding in Wysiwyg

--- changebyus/static/js/views/partials/ProjectUpdateFormView.js
+++ changebyus/static/js/views/partials/ProjectUpdateFormView.js
@@ -1,15 +1,23 @@
 define(["underscore", 
         "backbone", 
         "jquery", 
+        "bootstrap", 
         "template",
         "form",
+        "prettify",
+        "wysiwyg",
+        "hotkeys",
         "views/partials/ProjectSubView"],
 
     function(_, 
              Backbone, 
              $, 
+             bootstrap,
              temp, 
              form,
+             prettify,
+             wysiwyg,
+             hotkeys,
              ProjectSubView) {
     
     var ProjectUpdateFormView = ProjectSubView.extend({
@@ -30,20 +38,63 @@ define(["underscore",
             this.viewData = {project_id:window.projectID, response_id:"PLACEHOLDER"};
             this.$el = $("<div class='project-update-form'/>");
             this.$el.template(this.templateDir + '/templates/partials-project/project-update-form.html', {data:this.viewData}, function() {
-                self.ajaxForm();
+                self.jQueryForm();
             });
             $(this.parent).append(this.$el);
         },
 
-        ajaxForm:function(){ 
+        jQueryForm:function(){ 
             // AJAXIFY THE FORM
             var $updateForm = $('form[name="project-update"]');
             $updateForm.ajaxForm(function(response) { 
                 console.log(response);
             }); 
- 
+
+            function initToolbarBootstrapBindings() {
+              var fonts = ['Serif', 'Sans', 'Arial', 'Arial Black', 'Courier', 
+                    'Courier New', 'Comic Sans MS', 'Helvetica', 'Impact', 'Lucida Grande', 'Lucida Sans', 'Tahoma', 'Times',
+                    'Times New Roman', 'Verdana'],
+                    fontTarget = $('[title=Font]').siblings('.dropdown-menu');
+
+              $.each(fonts, function (idx, fontName) {
+                  fontTarget.append($('<li><a data-edit="fontName ' + fontName +'" style="font-family:\''+ fontName +'\'">'+fontName + '</a></li>'));
+              });
+
+              $('a[title]').tooltip({container:'body'});
+              
+              $('.dropdown-menu input').click(function() {return false;})
+                    .change(function () {$(this).parent('.dropdown-menu').siblings('.dropdown-toggle').dropdown('toggle');})
+                .keydown('esc', function () {this.value='';$(this).change();});
+
+              $('[data-role=magic-overlay]').each(function () { 
+                var overlay = $(this), target = $(overlay.data('target')); 
+                overlay.css('opacity', 0).css('position', 'absolute').offset(target.offset()).width(target.outerWidth()).height(target.outerHeight());
+              });
+              
+              if ("onwebkitspeechchange"  in document.createElement("input")) {
+                var editorOffset = $('#editor').offset();
+                $('#voiceBtn').css('position','absolute').offset({top: editorOffset.top-20, left: editorOffset.left+$('#editor').innerWidth()-75});
+              } else {
+                $('#voiceBtn').hide();
+              }
+            }
+
+            function showErrorAlert (reason, detail) {
+                var msg='';
+                if (reason==='unsupported-file-type') { msg = "Unsupported format " +detail; }
+                else {
+                    console.log("error uploading file", reason, detail);
+                }
+                $('<div class="alert"> <button type="button" class="close" data-dismiss="alert">&times;</button>'+ 
+                 '<strong>File upload error</strong> '+msg+' </div>').prependTo('#alerts');
+            }
+            
+            initToolbarBootstrapBindings();  
+            
+            $('#editor').wysiwyg({ fileUploadError: showErrorAlert} );
+            
+            window.prettyPrint && prettyPrint();
         }
- 
     });
 
     return ProjectUpdateFormView;

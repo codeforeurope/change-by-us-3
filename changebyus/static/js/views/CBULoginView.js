68d40af03e1861bf55a2a3a4448aa43915c48d93
Matt Lohmann <mattlohmann@localprojects.net>
2013-09-30 11:59:40 -0400

    login Ajaxed

--- changebyus/static/js/views/CBULoginView.js
+++ changebyus/static/js/views/CBULoginView.js
@@ -13,6 +13,7 @@ define(["underscore",
         parent: 'body',
         templateDir: '/static',
         viewData:{}, 
+        $submit:null,
 
         initialize: function(options) {
             this.templateDir = options.templateDir || this.templateDir;
@@ -22,9 +23,34 @@ define(["underscore",
         },
 
         render:function(){ 
+            var self = this;
             this.$el = $("<div class='login'/>");
-            this.$el.template(this.templateDir + '/templates/login.html', {data:this.viewData}, function() { });
+            this.$el.template(this.templateDir + '/templates/login.html', {data:this.viewData}, function() {
+                self.$submit = $('input[type="submit"]');
+                self.ajaxForm();
+             });
             $(this.parent).append(this.$el); 
+        },
+
+        ajaxForm:function(){
+            var self = this,
+                $login = $('form[name=signin]'),
+                $feedback = $('#login-feedback'),
+                options = { 
+                    beforeSubmit: function() { 
+                        self.$submit.prop('disabled', true);
+                        $feedback.removeClass('alert').html('');
+                    },
+                    success: function(response) { 
+                        self.$submit.prop('disabled', false);
+                        if (response.msg.toLowerCase() == "ok"){
+                            window.location.href = "/";
+                        }else{
+                            $feedback.addClass('alert').html(response.msg);
+                        }
+                    }
+                }
+            $login.ajaxForm(options); 
         }
     });
 

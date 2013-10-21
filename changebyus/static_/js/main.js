5974bd5c4a74626d88d31ac3128cba60e0ccb0a8
Matt Lohmann <mattlohmann@localprojects.net>
2013-10-21 11:33:57 -0400

    clean out old files

--- changebyus/static_/js/main.js
+++
@@ -1,5 +0,0 @@
-require(['ext/jquery', 'ext/underscore', 'ext/backbone', 'template', 'projectlist'], function() {
-    $(document).ready(function() {
-        window.App = new ProjectList({ appendTo: $('body') });
-    });
-});
